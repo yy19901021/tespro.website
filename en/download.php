58915a2cdc10de715713875f51a57b0a70dc8759
过欣炜 <guo.xinwei@topka.cn>
2014-09-06 12:17:43 +0800

    Update TP-USB driver.

--- en/download.php
+++ en/download.php
@@ -10,9 +10,13 @@
 <div id="Content"><!--Start of content-->
 	<div class="HalfWidth">
 		<h3>TP-USB Optical Probe Driver</h3>
-		<p>TP-USB compatible with USB2.0 standard. You can click following link to download the driver and installation guide.</p>
-		<p>System requirement: Windows 2000, Windows XP, Windows Vista, Windows 7.
-		<p><a href="en/download/TP-USB.zip">Download TP-USB.zip</a></p>
+		<ul>
+			<li><a href="zh/download/TP-USB-Windows.zip">Download Windows Driver</a></li>
+			<li><a href="zh/download/TP-USB-MacOSX.zip">Download Mac OS X Driver</a></li>
+			<li><a href="zh/download/TP-USB-Linux.zip">Download Linux Driver</a></li>
+			<li><a href="zh/download/TP-USB-WinCE.zip">Download Windows CE Driver</a></li>
+			<li><a href="zh/download/TP-USB-Android.zip">Download Android Driver</a></li>
+		</ul>
 	</div>
 </div><!--End of content-->
 <?php include("footer.php") ?>
