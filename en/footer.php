bb08110abe1e733113f4972784c1de63e79611b4
GUO Xinwei <xinwei.guo@gmail.com>
2018-07-06 23:36:15 +0800

    update copyright information on the footer.

--- en/footer.php
+++ en/footer.php
@@ -4,7 +4,7 @@
 				<div class="col-sm-12">
 				Tel: +86.1370.1122.354 ( English and Chinese )
 				<br />Email: <a href="m&#97;ilto:&#105;&#110;&#102;&#111;&#64;&#116;&#101;&#115;&#112;&#114;&#111;&#46;&#99;&#111;&#109;&#46;&#99;&#110;" title="Contact Tespro">&#105;&#110;&#102;&#111;&#64;&#116;&#101;&#115;&#112;&#114;&#111;&#46;&#99;&#111;&#109;&#46;&#99;&#110;</a>&nbsp;( For purchasing inquiry and general information )
-				<br />&copy;2007-2015 Tespro Electronics Co., Ltd.
+				<br />&copy;2007-2018 Tespro Electronics Co., Ltd.
 			</div>
 		</div>
 	</div>
